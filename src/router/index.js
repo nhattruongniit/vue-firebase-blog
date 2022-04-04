77d4113680ab0ef5d54eb0b5568bb9541f896045
TonyNguyen <truong.nn@amanotes.com>
2022-04-04 16:32:43 +0700

    doing login

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
 import Blogs from "@/views/Blogs.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+import ForgotPassword from "@/views/ForgotPassword.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +24,33 @@ const routes = [
     component: Blogs,
     meta: {
       title: "Blogs",
+      requiresAuth: false,
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {
+      title: "Login",
+      requiresAuth: false,
+    },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta: {
+      title: "Register",
+      requiresAuth: false,
+    },
+  },
+  {
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    meta: {
+      title: "ForgotPassword",
       requiresAuth: false,
     },
   },
